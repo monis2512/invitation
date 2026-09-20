f33b2aaf4ced7a9a09990d43e0495ea7d4307c3d
monis2512 <102420522+monis2512@users.noreply.github.com>
2026-09-20 09:35:55 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/dateinvite/DateInviteApplication.java
@@ -0,0 +1,11 @@
+package com.example.dateinvite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateInviteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DateInviteApplication.class, args);
+    }
+}
